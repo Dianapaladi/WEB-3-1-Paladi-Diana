fd3f914964fee1e6a12f0365029e23b424845732
Dianapaladi <diana.paladi15@gmail.com>
2026-09-04 08:28:02 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const button = document.getElementById("themeButton");
-
-button.addEventListener("click", function() {
-    document.body.classList.toggle("light-mode");
-    if(document.body.classList.contains("light-mode")) {
-        button.textContent = "🌙";
-    } else{
-        button.textContent = "☀️";
-    }});
